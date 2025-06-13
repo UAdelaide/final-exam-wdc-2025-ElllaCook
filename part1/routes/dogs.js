49048a5169cadfb8f39aa0a444b8da854ed9ab6e
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:19:46 +0000

    [automatic commit] 2025-06-13 06:19:46 UTC 61.68.204.32

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var 
+var db = require('../db');
 
 router.get('/', function(req, res) {
 
-    query = 'SELECT dog_name, size, owner_username FROM Dogs'
+    const query = 'SELECT dog_name, size, owner_username FROM Dogs'
 
 
   res.render('index', { title: 'Express' });
