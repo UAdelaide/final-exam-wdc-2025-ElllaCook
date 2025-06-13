d0be10c248a4cf152dc5762341413b290250d130
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:15:38 +0000

    [automatic commit] 2025-06-13 06:15:38 UTC 61.68.204.32

--- part1/app.js
+++ part1/app.js
@@ -21,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ROUTES AGAIN
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/dogs', dogsRouter);
