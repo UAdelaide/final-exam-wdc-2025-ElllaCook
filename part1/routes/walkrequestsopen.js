0134faac94a7e9283358467a99347133dc3a60e4
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 07:49:16 +0000

    [automatic commit] 2025-06-13 07:49:16 UTC ;; communications error to 208.67.222.222#53: timed out 61.68.204.32

--- part1/routes/walkrequestsopen.js
+++ part1/routes/walkrequestsopen.js
@@ -9,3 +9,8 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time,
+WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
+FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id JOIN Users ON Dogs.owner_id = Users.user_id
+WHERE WalkRequests.status = 'open'`
