c693645e493857e8e4f1745f02bb423b77381444
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 07:11:21 +0000

    [automatic commit] 2025-06-13 07:11:21 UTC 61.68.204.32

--- part1/routes/walkerssummary.js
+++ part1/routes/walkerssummary.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+    try {
+    const [rows] = await db.query(`SELECT Dogs.name AS dog_name, Dogs.size,
+    Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id`
+    );
+    res.json(rows);
+} catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'DB error' });
+        }
 });
 
 module.exports = router;
