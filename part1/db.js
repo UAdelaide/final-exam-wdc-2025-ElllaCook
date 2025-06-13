76642afe16bbec051c31c481526c2ad8c8fdde4c
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:12:33 +0000

    [automatic commit] 2025-06-13 06:12:33 UTC 61.68.204.32

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,9 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root'
+    user: 'root',
     password: 'root',
     database: 'DogWalkService'
-})
+});
+
+connection.connect((err) )
