d39c5a2aa2925cdb3811c02fa90027882efcea87
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 07:53:51 +0000

    [automatic commit] 2025-06-13 07:53:51 UTC 61.68.204.32

--- part1/routes/walkerssummary.js
+++ part1/routes/walkerssummary.js
@@ -6,7 +6,9 @@ var db = require('../db');
 router.get('/', async (req, res) => {
     try {
     const [rows] = await db.query(`SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id) AS total_ratings,
-        ROUND(AVG(WalkRatings.rating), 2) AS average_rating, COUNT(DISTINCT WalkRequests.request_id) AS completed_walks FROM Users LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id LEFT JOIN WalkRequests ON WalkRatings.request_id = WalkRequests.request_id AND WalkRequests.status = 'completed' WHERE Users.role = 'walker'
+        ROUND(AVG(WalkRatings.rating), 2) AS average_rating, COUNT(DISTINCT WalkRequests.request_id) AS completed_walks
+        FROM Users LEFT JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id LEFT JOIN WalkRequests ON WalkRatings.request_id = WalkRequests.request_id
+        AND WalkRequests.status = 'completed' WHERE Users.role = 'walker'
         GROUP BY Users.username;`
     );
     res.json(rows);
