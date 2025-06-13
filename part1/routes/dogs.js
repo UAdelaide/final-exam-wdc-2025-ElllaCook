ce4046e0e398687311cce22146eda72871f2c5bd
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:27:41 +0000

    [automatic commit] 2025-06-13 06:27:41 UTC 61.68.204.32

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -7,7 +7,10 @@ router.get('/', async (req, res) => {
     const [rows] = await db.query()`SELECT Dogs.name AS dog_name, Dogs.size,
     Users.username FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id`
     );
-
+    res.json(rows);
+} catch (err) {
+    
+}
     try {
     db.query(sql, (err, result) => {
         res.json(result)
