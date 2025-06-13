c89fe38be04dfeeaf4951fcff6fb8e88b13f7317
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 07:49:37 +0000

    [automatic commit] 2025-06-13 07:49:37 UTC 61.68.204.32

--- part1/routes/walkrequestsopen.js
+++ part1/routes/walkrequestsopen.js
@@ -3,14 +3,17 @@ var router = express.Router();
 var db = require('../db');
 
 
-
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
-
-`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time,
+router.get('/', async (req, res) => {
+    try {
+    const [rows] = await db.query(`SELECT WalkRequests.request_id, Dogs.name AS dog_name, WalkRequests.requested_time,
 WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username
 FROM WalkRequests JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id JOIN Users ON Dogs.owner_id = Users.user_id
 WHERE WalkRequests.status = 'open'`
+    );
+    res.json(rows);
+} catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'DB error' });
+        }
+});
+module.exports = router;
