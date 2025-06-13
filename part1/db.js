fc0e5d79bc9f4e450979b7398d446a835690a9ff
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:42:31 +0000

    [automatic commit] 2025-06-13 06:42:31 UTC 61.68.204.32

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,10 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'DogWalkService'
+  database: 'DogWalkService',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
