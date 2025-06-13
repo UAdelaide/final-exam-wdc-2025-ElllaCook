66f46ed47ae16ff185a856bcc97608b284ab02d3
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 06:14:16 +0000

    [automatic commit] 2025-06-13 06:14:16 UTC 61.68.204.32

--- part1/app.js
+++ part1/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var db = require('./db');
+
+// DB connection
+var db require(./db.js)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dogsRouter = require('.routes/dogs');
