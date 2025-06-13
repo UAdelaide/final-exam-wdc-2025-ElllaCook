5f8fa1a544714da8299e13ed69793deb032133ef
Elllacook <ella.cook.ec@gmail.com>
2025-06-13 07:46:26 +0000

    [automatic commit] 2025-06-13 07:46:26 UTC ;; communications error to 208.67.222.222#53: timed out 61.68.204.32

--- part1/routes/walkrequestsopen.js
+++ part1/routes/walkrequestsopen.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
-/* GET users listing. */
+
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
