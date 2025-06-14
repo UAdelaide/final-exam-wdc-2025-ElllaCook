5254365d39b3d3417ecdc3d35dc2b9945d9235e9
Elllacook <ella.cook.ec@gmail.com>
2025-06-14 05:37:52 +0000

    [automatic commit] 2025-06-14 05:37:52 UTC 61.68.204.32

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -1,1 +1,18 @@
 
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+router.get('/', async (req, res) => {
+    try {
+    const [rows] = await db.query(`SELECT Dogs.name AS dog_name, Dogs.size,
+    Users.username AS owner_username FROM Dogs JOIN Users ON Dogs.owner_id = Users.user_id`
+    );
+    res.json(rows);
+} catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'DB error' });
+        }
+});
+
+module.exports = router;
