b8eeee567aaf6ab4b4e3b15d9411c6f6e63dcae0
Elllacook <ella.cook.ec@gmail.com>
2025-06-14 04:44:03 +0000

    [automatic commit] 2025-06-14 04:44:03 UTC 61.68.204.32

--- part2/routes/dashboardRoutes.js
+++ part2/routes/dashboardRoutes.js
@@ -10,7 +10,7 @@ router.get('/owner'), (req, res) => {
         return res.redirect('/');
     }
     if (req.session.user.role !== 'owner'){
-        return res.status.(403).send('Not authorised')
+        return res.status.(403).send('Not authorised'); // incorrect role
     }
     res.sendFile(path.join(__dirname, '../public/owner-dashboard.html'));
 });
