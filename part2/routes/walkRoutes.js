e39129f57871271a916bc756b7a4675258fb0806
Elllacook <ella.cook.ec@gmail.com>
2025-06-14 04:58:11 +0000

    [automatic commit] 2025-06-14 04:58:11 UTC 61.68.204.32

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -67,7 +67,9 @@ router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(
       `SELECT dog_id, name FROM Dogs WHERE owner_id = ?`,
-    )
+      [owner_id]
+    );
+    res.json(rows);
   }
 })
 module.exports = router;
