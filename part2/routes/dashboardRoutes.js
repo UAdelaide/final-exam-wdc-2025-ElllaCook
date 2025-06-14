00cd1cf74ea3e1720441971ec1bddbb33a904750
Elllacook <ella.cook.ec@gmail.com>
2025-06-14 04:40:16 +0000

    [automatic commit] 2025-06-14 04:40:16 UTC 61.68.204.32

--- part2/routes/dashboardRoutes.js
+++ part2/routes/dashboardRoutes.js
@@ -6,8 +6,8 @@ const { path } = require('../app');
 
 // WALKER DASH ROUTER
 router.get('/walker'), (req, res) => {
-    res.sendFile(path.join(__dirname, ))
-}
+    res.sendFile(path.join(__dirname, '../public/owner-dashboard.html'));
+};
 // OWNER DASH ROUTER
 
 module.exports = router;
