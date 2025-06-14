4e6eddb1a2a8367b65ede512a18b759bc1e52cb9
Elllacook <ella.cook.ec@gmail.com>
2025-06-14 04:41:59 +0000

    [automatic commit] 2025-06-14 04:41:59 UTC 61.68.204.32

--- part2/routes/dashboardRoutes.js
+++ part2/routes/dashboardRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
-const { path } = require('app');
+const path = require('path');
 
 
 // OWNER DASH ROUTER
